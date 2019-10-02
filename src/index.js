0525525f90d6277acc1e9ba47d4920387d0840d9
Dmitry Kim <d.kim2009@tut.by>
2019-10-02 22:30:46 +0300

    feat: solved brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let pre = Array.from(bracketsConfig);
+    let rules = {};
+    for (let i=0; i<pre.length; i++) {
+        let prep = pre[i];
+        for (let j=0; j<prep.length; j+=2) {
+            rules[prep[j]] = prep[j+1];
+        }
+    }
+
+    let stack = new Array();
+
+    let str_arr = Array.from(str);
+    for (let i=0; i<str_arr.length; i++) {
+        if (rules[str_arr[i]] != undefined) {
+            let data = stack.pop();
+            if (data != undefined) stack.push(data);
+            if ((str_arr[i] == "|" || str_arr[i] == "7" || str_arr[i] == "8") && str_arr[i] == data) stack.pop();
+            else stack.push(str_arr[i]);
+        }
+        else if (stack.length == 0) return false;
+            else {
+                let data = stack.pop();
+                stack.push(data);
+                if (rules[data] == str_arr[i]) stack.pop();
+                else return false;
+            }
+    }
+    if (stack.length != 0) return false;
+    return true;
 }
